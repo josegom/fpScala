d1bbc45d3e324f0f682a93698c88a7376e697c6b
jose manuel gomez <josegom@gmail.com>
2016-02-14 13:29:07 +0100

    List exercises

---
+++ src/main/scala/chapter3/List.scala
@@ -0,0 +1,97 @@
+package chapter3
+
+sealed trait List[+A] // `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
+which may be `Nil` or another `Cons`.
+ */
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match {
+    // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+/**Mine **/
+
+def tail[A](as : List[A]) : List[A] = as match{
+  case Nil => Nil
+  case Cons(x,xs) => xs
+  case _ => Nil
+}
+
+  def setHead[A](l: List[A], h: A): List[A] = h match {
+    case Nil => l
+    case _ => Cons(h,l)
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] ={
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+    }
+  }
+
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+      l match {
+        case Nil => Nil
+        case Cons(h,t) => {
+          if (f(h)) dropWhile(t,f)
+          else l
+        }
+      }
+    }
+
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h,Nil) => Nil
+      case Cons(h,t) => Cons(h, init(t))
+
+    }
+  }
+  /** No More Mine **/
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+
+
+
+
+
+
+
+  def length[A](l: List[A]): Int = sys.error("todo")
+
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+
+  def map[A, B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+}
---
+++ src/test/scala/chapter3/ListTest.scala
@@ -0,0 +1,78 @@
+package chapter3
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by jose on 13/02/2016.
+  */
+@RunWith(classOf[JUnitRunner])
+class ListTest extends FlatSpec with Matchers {
+
+
+  val orderedList = List (Array(1,2,3,4), Array(1,1,1,1,1), Array(1,5,7,8,11), Array(1,2,2,4,6,7,7), Array(1))
+  val disorganizedList = List (Array(2,1), Array(2,2,2,1), Array(1,2,3,4,1), Array(7,1,2,3,4))
+
+  "I " should "wish a match guess" in {
+
+    val x = List(1,2,3,4,5) match {
+      case Cons(x,Cons(2, Cons(4,_))) => x
+      case Nil => 42
+      case Cons(x,Cons(y,Cons(3,Cons(4,_)))) => x + y
+      case Cons(h,t) => h + sum(t)
+      case _ => 101
+    }
+
+    x should be(3)
+  }
+
+  "A tail in nil List" should "return nil" in {
+    List.tail(Nil) should be(Nil)
+  }
+
+  "one member List tail " should "return nil" in {
+    List.tail(List("x")) should be(Nil)
+  }
+
+  "List tail" should "return tail" in {
+    List.tail(List("x","y","z")) should be(List("y","z"))
+  }
+
+
+  "A head in a nil List" should "return the element" in {
+    List.setHead(Nil,1) should be(List(1))
+  }
+
+  "A head in a List" should "return the list with a new head" in {
+    List.setHead(List(1),0) should be(List(0,1))
+  }
+
+  "A head in a List with nil" should "return the same list" in {
+    val list= List(1)
+    List.setHead(list,Nil) should be(list)
+  }
+
+  "A drop 2 " should "return a list without 2 elements" in {
+    List.drop(List(1,2,3,4,5),2) should be (List(3,4,5))
+  }
+
+  "A drop while" should "drop while the condition is true" in {
+    List.dropWhile(List(1,1,1,2,1,1), (x: Int)=>x==1) should be(List(2,1,1))
+  }
+
+  "Wit init" should "create a list without the last element" in {
+    List.init(List(1,2,3,4,5,6)) should be(List(1,2,3,4,5))
+
+  }
+
+
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x,xs) => x + sum(xs)
+  }
+
+
+
+}
